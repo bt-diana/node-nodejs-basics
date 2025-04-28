b75d2b18b31cffee42d21f97d205f96f5b2f438d
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-29 00:10:06 +0600

    feat: implement streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from 'fs';
+import { resolve } from 'path';
+import { createInterface } from 'readline';
+
+const __dirname = import.meta.dirname;
+
 const write = async () => {
-    // Write your code here 
+    const filePath = resolve(__dirname, 'files', 'fileToWrite.txt');
+
+    const writeStream = createWriteStream(filePath);
+    const rl = createInterface(process.stdin);
+
+    rl.on('line', (input) => {
+        writeStream.write(input);
+        writeStream.write('\n');
+    });
 };
 
 await write();
