d94f0de70f1747ff28961e837d1969c10622bb06
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-28 18:31:03 +0600

    feat: implement fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { open } from 'node:fs/promises';
+import { resolve } from 'node:path';
+const __dirname = import.meta.dirname;
+
 const create = async () => {
-    // Write your code here 
+    const filePath = resolve(__dirname, 'files', 'fresh.txt');
+
+    let file;
+    try {
+        file = await open(filePath, 'wx');
+        file.write("I am fresh and young");
+    } catch (e) {
+        if (e.code === 'EEXIST') {
+            throw Error('FS operation failed');
+        } else {
+            throw e;
+        }
+    } finally {
+        await file?.close();
+    }
 };
 
 await create();
