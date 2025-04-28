7d105b2e39eba4b483b12fde8d43b320b53b475c
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-28 18:57:11 +0600

    feat: implement fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+const __dirname = import.meta.dirname;
+
 const read = async () => {
-    // Write your code here 
+    const fileToReadPath = resolve(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const fileContent = await readFile(fileToReadPath);
+        console.log(fileContent.toString());
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            throw Error('FS operation failed');
+        } else {
+            throw e;
+        }
+    }
 };
 
 await read();
