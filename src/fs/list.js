cfc27d7a6f76688def98043659687de36a1bb2f2
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-28 18:37:19 +0600

    feat: implement fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { readdir } from 'node:fs/promises';
+import { resolve } from 'node:path';
+const __dirname = import.meta.dirname;
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = resolve(__dirname, 'files');
+
+    try {
+        const files = await readdir(dirPath, {withFileTypes: true});
+        files.forEach((file) => {
+            console.log(file.name);
+        });
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            throw Error('FS operation failed');
+        } else {
+            throw e;
+        }
+    }
 };
 
 await list();
