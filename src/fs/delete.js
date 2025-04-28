1d4a4642693abb03fd38a640a2090e1ebfc4181d
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-28 18:35:12 +0600

    feat: implement fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { unlink } from 'node:fs/promises';
+import { resolve } from 'node:path';
+const __dirname = import.meta.dirname;
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemove = resolve(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await unlink(fileToRemove);
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            throw Error('FS operation failed');
+        } else {
+            throw e;
+        }
+    }
+    
 };
 
 await remove();
