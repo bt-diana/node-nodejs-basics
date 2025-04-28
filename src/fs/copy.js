030ae9fd1fe9c2b5dc26dc2e5d7f48f5cae52922
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-28 18:31:14 +0600

    feat: implement fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,38 @@
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+const __dirname = import.meta.dirname;
+
 const copy = async () => {
-    // Write your code here 
+    const dirPath = resolve(__dirname, 'files');
+    const newDirPath = resolve(__dirname, 'files-copy');
+    let files;
+
+    try {
+        files = await readdir(dirPath, {withFileTypes: true});
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            throw Error('FS operation failed');
+        } else {
+            throw e;
+        }
+    }
+
+    try {
+        await mkdir(newDirPath);
+    } catch (e) {
+        if (e.code === 'EEXIST') {
+            throw Error('FS operation failed');
+        } else {
+            throw e;
+        }
+    }
+
+    files.forEach((file) => {
+        copyFile(
+            resolve(dirPath, file.name),
+            resolve(newDirPath, file.name),
+        );
+    });
 };
 
 await copy();
