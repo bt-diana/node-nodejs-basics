5415f398480fd44a2521a5e6b85b85bf685ef15b
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-29 00:27:00 +0600

    feat: implement modules/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from 'node:stream';
+
+const reverseTranform = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(null, chunk.toString().split('').reverse().join('') + '\n\n');
+  }
+});
+
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverseTranform).pipe(process.stdout);
 };
 
 await transform();
