cca8f48f4f019e70c3719a3c9fad78757a04ec51
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-29 01:24:53 +0600

    feat: implement zip/compress.js and zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { unlink } from 'node:fs/promises';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { resolve } from 'node:path';
+const __dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = resolve(__dirname, 'files', 'fileToCompress.txt');
+    const arcPath = resolve(__dirname, 'files', 'archive.gz');
+
+    const gunzip = createGunzip();
+    const source = createReadStream(arcPath);
+    const destination = createWriteStream(filePath);
+
+    await pipeline(source, gunzip, destination);
+    await unlink(arcPath);
 };
 
 await decompress();
